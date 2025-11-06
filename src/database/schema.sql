7ef16cde0a279f82a6112db841e2744f25b678d6
Evelyn Oliveira <evelynoliveira.tds1.senai@gmail.com>
2025-11-06 15:18:11 -0300

    feat: create schema

---
+++ src/database/schema.sql
@@ -0,0 +1,124 @@
+CREATE DATABASE expresso_stories;
+\c expresso_stories;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    phone VARCHAR(13) NOT NULL,
+    password_hash TEXT NOT NULL,
+    photo TEXT
+);
+
+CREATE TABLE user_addresses (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    cep VARCHAR(9) NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    number VARCHAR(20) NOT NULL,
+    neighborhood VARCHAR(100) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    state VARCHAR(2) NOT NULL, 
+    complement TEXT,
+    reference_point TEXT,
+    is_default BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+);
+
+CREATE TABLE branches (
+    id SERIAL PRIMARY KEY
+    name VARCHAR(255) NOT NULL
+    cep VARCHAR(9) NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    number VARCHAR(20) NOT NULL,
+    neighborhood VARCHAR(100) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    state VARCHAR(2) NOT NULL, 
+    complement TEXT,
+    reference_point TEXT
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+    name VARCHAR(255) NOT NULL,
+    photo TEXT NOT NULL,
+    description TEXT,
+    price DECIMAL(10,2) NOT NULL,
+    inspiration TEXT,
+    photo_inspiration TEXT
+);
+
+CREATE TABLE feature_products (
+    id SERIAL PRIMARY KEY,
+    branch_id INTEGER REFERENCES branches(id) ON DELETE CASCADE,
+    category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+    name VARCHAR(255) NOT NULL,
+    photo TEXT NOT NULL,
+    description TEXT,
+    price DECIMAL(10,2) NOT NULL,
+    inspiration TEXT,
+    photo_inspiration TEXT
+);
+
+CREATE TABLE carts(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    branch_id INTEGER REFERENCES branches(id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE cart_items (
+    id SERIAL PRIMARY KEY,
+    cart_id INTEGER REFERENCES carts(id) ON DELETE CASCADE,
+    product_id INTEGER REFERENCES products(id) ON DELETE CASCADE,
+    featured_product_id INTEGER REFERENCES featuredproduct_branches(id) ON DELETE CASCADE,
+    quantity INTEGER NOT NULL DEFAULT 1,
+    price DECIMAL(10,2) NOT NULL,
+    observations TEXT
+);
+
+CREATE TYPE payment_method_enum AS ENUM ('pix', 'cartao', 'dinheiro');
+
+CREATE TYPE order_status_enum AS ENUM ('Pendente', 'Pago', 'Cancelado');
+
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    branch_id INTEGER REFERENCES branches(id) ON DELETE CASCADE,
+    user_address_id INTEGER REFERENCES user_addresses(id) ON DELETE CASCADE,
+    request_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    payment_method payment_method_enum NOT NULL,
+    payment_status VARCHAR(50) DEFAULT 'Pendente',
+    status order_status_enum DEFAULT 'Pendente',
+    total_value DECIMAL(10,2),
+    observations TEXT
+);
+
+CREATE TABLE order_items (
+    id SERIAL PRIMARY KEY,
+    order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+    featuredproduct_branches INTEGER REFERENCES featuredproduct_branches(id) ON DELETE CASCADE,
+    product_id INTEGER REFERENCES products(id) ON DELETE CASCADE,
+    quantity INTEGER NOT NULL,
+    price DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    product_id INTEGER REFERENCES products(id) ON DELETE CASCADE,
+    note INTEGER CHECK(note BETWEEN 1 AND 5),
+    comments TEXT,
+    evaluation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
